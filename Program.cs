c5bc75a992e3be4360693ed97cc1d8b472695bff
Kevin Richter Krogsgaard Andersen <88718302+Keveren1@users.noreply.github.com>
2021-11-22 10:26:08 +0100

    Hey

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace MalgreTout
         {
             CreateHostBuilder(args).Build().Run();
         }
+        //bla bla
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
