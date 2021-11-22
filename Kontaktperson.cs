0fe24f0b7d32223d1520036b2accf3c46c78f484
Emma Bang Creutzberg <emma213r@edu.zealand.dk>
2021-11-22 12:07:31 +0100

    har lavet kontaktperson klassen

---
+++ Kontaktperson.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MalgreTout
+{
+    public class Kontaktperson
+    {
+        public int Id { get; set; }
+        public string Person { get; set; }
+        public int Tlf {get; set;}
+        public string Mail { get; set; }
+
+        public Kontaktperson (int id, string person, int tlf, string mail)
+        {
+            Id = id;
+            Person = person;
+            Tlf = tlf;
+            Mail = mail;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Person: {Person}, Tlf: {Tlf}, Mail: {Mail}";
+        }
+    }
+}
