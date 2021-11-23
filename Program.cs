1996285a6114851680da25c85305b9b34f296353
Kevin Richter Krogsgaard Andersen <88718302+Keveren1@users.noreply.github.com>
2021-11-23 10:08:47 +0100

    hey

--- Program.cs
+++ Program.cs
@@ -14,9 +14,13 @@ namespace MalgreTout
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-        }
-        //bla bla 
 
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
